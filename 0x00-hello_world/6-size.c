802222cdce571204497cc81a28ed199b26b7edd2
Maxwell <duedumaxwell43@gmail.com>
2022-04-07 23:15:14 +0000

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -20,3 +21,4 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
+}
