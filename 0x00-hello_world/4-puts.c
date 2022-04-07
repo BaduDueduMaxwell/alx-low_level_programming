f3de1f24e4e7dfa93bfaadc452aa0bf021bf2554
Maxwell <duedumaxwell43@gmail.com>
2022-04-07 15:43:27 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <studio.h>
-
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {	puts("\"Programming is like building a multilingual puzle":);
-	return (0);
-}	
+	return (0);}}	
