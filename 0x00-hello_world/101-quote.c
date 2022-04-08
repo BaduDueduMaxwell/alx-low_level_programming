6814d37e500fbabd5891956974e5aa5b27747ca7
Maxwell <duedumaxwell43@gmail.com>
2022-04-07 19:54:18 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - print exactly the dataand return 1
+ * main - print exactly the date and return 1
  *
- * Return: program returns 1
+ * Return: program returnd 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
 	return (1);
-}
