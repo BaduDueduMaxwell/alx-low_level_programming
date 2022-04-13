3a1467d925caa7e48e4508866e2fb903d450a357
Maxwell <duedumaxwell43@gmail.com>
2022-04-12 21:20:47 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * jack_baver - Prints every minute of the day of
- *              Jack Baver, starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
-void jack_bauer(void)
+void jack_baver(void)
 {
 int hour, minute;
 
