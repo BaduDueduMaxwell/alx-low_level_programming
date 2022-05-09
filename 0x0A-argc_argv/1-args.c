cd6dd5545f4459e8de81c71a591c2540a3ea572d
Maxwell <duedumaxwell43@gmail.com>
2022-05-09 14:44:43 -0700

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,15 +3,15 @@
 
 /**
  * main - Entry point
- *
  * @argc: Counts the number of parameters that go into main
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)    
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	(void) argv;
-	printf("%i\n", argc - 1);
+	if (argv[0] != 0)
+		printf("%d\n", argc - 1);
+
 	return (0);
 }
-
