ae37669ebbf068b21704b2b45caf64ae16f8b620
Badu Duedu Maxwell <102438296+BaduDueduMaxwell@users.noreply.github.com>
2022-06-08 21:46:33 +0000

    100-get

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian or 1 if little endian
+ */
+int get_endianness(void)
+{
+unsigned int a;
+char *ch;
+	a = 1;
+	ch = (char *) &a;
+
+	return ((int)*ch);
+}
