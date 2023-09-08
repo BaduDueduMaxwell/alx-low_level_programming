e9ada60def90dd1e0fd9fbf8925e87fccaa7aec0
kode <duedumaxwell43@gmail.com>
2023-09-08 05:49:29 -0700

    print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main-prints a string of char
+ *
+ * return: 0 if successful
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
