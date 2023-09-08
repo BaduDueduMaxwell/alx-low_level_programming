e5ecb75a557d9b994ae365b64e44f5381022f8e1
kode <duedumaxwell43@gmail.com>
2023-09-08 07:57:02 -0700

    printed with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints a string of char
- *
- * Return 0: if successful
+ * main - Prints string
+ * Return: 0 if successful
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
