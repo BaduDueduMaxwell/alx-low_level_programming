1954ab926ece35a4bec6a692dd408f58113fdffa
kode <duedumaxwell43@gmail.com>
2023-09-10 10:27:14 -0700

    task 8:print nums and letters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-#include <limits.h>
 
 /**
- * main - nums and letters
+ * main - prints numbers and letters
  *
- * Return: 0 if successful
+ * Return: Always 0
  */
 int main(void)
 {
-        int num;
-        char letter;
+    int num;
+    char letter;
 
-        for (num = 0; num < 10; num++)
-        {
-                putchar(num + '0');
-        }
-        for (letter = 'a'; letter <= 'f'; letter++)
-        {
-                putchar(letter);
-        }
-        putchar('\n');
+    for (num = 0; num < 10; num++)
+    {
+        putchar(num + '0');
+    }
 
-        return (0);
+    for (letter = 'a'; letter <= 'f'; letter++)
+    {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return (0);
 }
