6faf690256e0478ca781adf468381188b1628350
kode <duedumaxwell43@gmail.com>
2023-09-10 05:12:03 -0700

    task 3:upper and lower letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,11 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
+	}
+       	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
 		putchar(letter);
 	}
 	putchar('\n');
