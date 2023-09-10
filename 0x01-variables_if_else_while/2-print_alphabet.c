237a9eed701171154e71965da9eac2603365014a
kode <duedumaxwell43@gmail.com>
2023-09-10 04:24:07 -0700

    task 2:printing alphabets lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets from a-z; lowercase
+ *
+ * Return: 0 if successful 
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
