77520af0864e0d174850405ab4374620b66c8f7b
kode <duedumaxwell43@gmail.com>
2023-09-12 18:57:02 -0700

    task 16: 98 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,23 @@
  */
 int main(void)
 {
-    unsigned int i;
-    unsigned int a = 1;
-    unsigned int b = 2;
-    unsigned int next;
+	unsigned int i;
+	unsigned int a = 1;
+	unsigned int b = 2;
+	unsigned int next;
 
-    printf("%u, %u", a, b);
+	printf("%u, %u", a, b);
 
-    for (i = 3; i <= 98; i++)
-    {
-        next = a + b;
-        printf(", %u", next);
-        a = b;
-        b = next;
-    }
+	for (i = 3; i <= 98; i++)
+	{
+		next = a + b;
+		printf(", %u", next);
+		a = b;
 
-    printf("\n");
+		b = next;
+	}
 
-    return (0);
+	printf("\n");
+
+	return (0);
 }
