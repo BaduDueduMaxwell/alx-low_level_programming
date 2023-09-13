0f051b746e0ac9913433b6c37cfc93baf158f024
Badu Duedu Maxwell <102438296+BaduDueduMaxwell@users.noreply.github.com>
2023-09-13 02:23:00 +0000

    task 12:print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,55 +8,45 @@
 
 void print_times_table(int n)
 {
+	int a = 0, b, c;
+
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
-
-	for (int a = 0; a <= n; a++)
+	while (a <= n)
 	{
-		print_row(a, n);
-	}
-}
-
-void print_row(int a, int n)
-{
-	for (int b = 0; b <= n; b++)
-	{
-		int c = a * b;
-		print_entry(c);
-
-		if (b != n)
+		for (b = 0; b <= n; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-}
+			c = a * b;
+			if (c > 99)
+			{
+				_putchar(c / 100 + '0');
+				_putchar((c / 10 % 10) + '0');
+				_putchar(c % 10 + '0');
+			}
+			else if (c > 9)
+			{
+				_putchar(' ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else if (b != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+				_putchar(c + '0');
 
-void print_entry(int c)
-{
-	if (c > 99)
-	{
-		_putchar(c / 100 + '0');
-		_putchar((c / 10 % 10) + '0');
-		_putchar(c % 10 + '0');
-	}
-	else if (c > 9)
-	{
-		_putchar(' ');
-		_putchar(c / 10 + '0');
-		_putchar(c % 10 + '0');
-	}
-	else if (c > 0)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(c + '0');
-	}
-	else
-	{
-		_putchar('0');
+			if (b != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		a++;
 	}
 }
