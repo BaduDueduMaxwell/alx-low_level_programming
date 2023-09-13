af3b8b341ccb5bd95766d55df762fd6364bc103d
kode <duedumaxwell43@gmail.com>
2023-09-12 19:04:31 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	long long int prev = 1, curr = 2, next, sum = 0;
+
+	while (curr <= 4000000) {
+		if (curr % 2 == 0) {
+			sum += curr;
+		}
+		next = prev + curr;
+		prev = curr;
+		curr = next;
+	}
+
+	printf("The sum of even-valued Fibonacci terms not exceeding 4,000,000
+ is: %lld\n", sum);
+
+	return 0;
+}
