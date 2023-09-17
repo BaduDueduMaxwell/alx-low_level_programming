ee495d68231681694138209778eb530e2bcfac9f
kode <duedumaxwell43@gmail.com>
2023-09-17 01:14:52 -0700

    task 3:prints the num from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers from 0-9
+ * @void - character
+ * Return: prints numbers with _putchar
+ */
+void print_numbers(void)
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+	}
+	putchar('\n');
+}
