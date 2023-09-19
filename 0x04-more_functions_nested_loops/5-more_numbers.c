33899f2d33a2daafad29cb2c2256b0de6453e0fa
kode <duedumaxwell43@gmail.com>
2023-09-19 09:11:46 -0700

    task 5:prints 10times numbers, from 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * void_more_numbers(void) - prints 10 times the num, 0-14
+ * @void - character
+ * Return: 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+                int num;
+
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				putchar('0' + num / 10);
+			}
+			putchar('0' + num % 10);
+                }
+		putchar('\n');
+		count++;
+	}
+}
