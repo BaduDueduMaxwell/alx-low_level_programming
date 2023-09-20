99489984911ff1fc0bc67c4d26760a8aefafff81
kode <duedumaxwell43@gmail.com>
2023-09-19 17:43:16 -0700

    task 9:fizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz
+ * @void: character
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+		putchar(' ');
+	}
+	return (0);
+}
