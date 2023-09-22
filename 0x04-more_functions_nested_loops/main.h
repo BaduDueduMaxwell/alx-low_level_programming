255f902143fada09a0f0f93db91e0b425ea02bcb
kode <duedumaxwell43@gmail.com>
2023-09-22 08:54:42 -0700

    task 12:print integers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void _putchar(int c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -11,5 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif
