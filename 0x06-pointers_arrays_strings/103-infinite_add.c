65bbbd0b1f6d3da9954b667003dcc5467290dfbf
kode <duedumaxwell43@gmail.com>
2023-09-22 18:42:26 -0700

    task 11:a\dvance

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,44 +12,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, carry = 0, sum;
-    int i, j;
+	int len1 = 0, len2 = 0, carry = 0, sum;
+	int i, j;
 
-    while (n1[len1] != '\0')
-        len1++;
-    while (n2[len2] != '\0')
-        len2++;
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
 
-    if (len1 + 1 > size_r || len2 + 1 > size_r)
-        return (0);
+	if (len1 + 1 > size_r || len2 + 1 > size_r)
+		return (0);
+	len1--;
+	len2--;
+	r[size_r--] = '\0';
 
-    len1--;
-    len2--;
-    r[size_r--] = '\0';
+	for (i = len1, j = len2; i >= 0 || j >= 0; i--, j--, size_r--)
+	{
+		sum = carry;
 
-    for (i = len1, j = len2; i >= 0 || j >= 0; i--, j--, size_r--)
-    {
-        sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
 
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
+		carry = sum / 10;
+		r[size_r] = (sum % 10) + '0';
+	}
 
-        carry = sum / 10;
-        r[size_r] = (sum % 10) + '0';
-    }
+	if (carry)
+	{
+		if (size_r == 0)
+			return (0);
+		r[size_r] = carry + '0';
+	}
+	else
+	{
+		size_r++;
+	}
 
-    if (carry)
-    {
-        if (size_r == 0)
-            return (0);
-        r[size_r] = carry + '0';
-    }
-    else
-    {
-        size_r++;
-    }
-
-    return (r + size_r);
+	return (r + size_r);
 }
