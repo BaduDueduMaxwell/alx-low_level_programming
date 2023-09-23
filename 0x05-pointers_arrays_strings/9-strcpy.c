b6b3536a6191db9acb617c035e430ae3091fcd69
Badu Duedu Maxwell <102438296+BaduDueduMaxwell@users.noreply.github.com>
2023-09-23 00:11:06 +0000

    task 9: copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,26 +1,15 @@
+#include <stdio.h>
+#include <string.h>
 #include "main.h"
-
 /**
- * _strcpy - copies string including terminating byte
- * @dest: first pointer
- * @src: second pointer
- * Return: dest if successful.
+ *_strcpy - check the code
+ *
+ *@dest: a pointer variable
+ *@src: another pointer variable
+ *
+ *Return: a pointer
  */
-
 char *_strcpy(char *dest, char *src)
 {
-	int i, j;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-
-	for (j = 0; j <= i; j++)
-	{
-		dest[j] = src[j];
-	}
-
-	return (dest);
+	return (strcpy(dest, src));
 }
