087bcbe11deb83d74b7406b69615b484466c9535
kode <duedumaxwell43@gmail.com>
2023-09-27 10:04:00 -0700

    task 0:prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: pointer
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
