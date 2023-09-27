56c971888ce1a69b1e4a92c519f799e392ff9c8d
kode <duedumaxwell43@gmail.com>
2023-09-27 14:03:35 -0700

    task 1:reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,26 +1,16 @@
 #include "main.h"
 #include <string.h>
-
+#include <stdio.h>
 /**
  * _print_rev_recursion: prints reverse of a string
  * @s: pointer variable
  */
 void _print_rev_recursion(char *s)
 {
-	int start = 0;
-	int end = strlen(s) - 1;
-
-	if (!s)
+	if (*s)
 	{
-		return;
-	}
+		_print_rev_recursion(s + 1);
 
-	while (start < end)
-	{
-		char c = s[start];
-		s[start] = s[end];
-		s[end] = c;
-		start++;
-		end--;
+		putchar(*s);
 	}
 }
