89a6397efe4d97e6612a42e9f41b3e6ff864ad0e
kode <duedumaxwell43@gmail.com>
2023-10-01 03:51:42 -0700

    task 0:print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,15 @@
 
 /**
  * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
  * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i;
+	(void)argc;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
-	printf("\n");
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
