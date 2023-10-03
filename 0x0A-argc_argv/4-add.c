6f2b0cd02f3dcef4bed72884862335aaf4fc1815
Badu Duedu Maxwell <102438296+BaduDueduMaxwell@users.noreply.github.com>
2023-10-03 09:56:01 +0000

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
- * main - adds positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success and 1 on error
+ * main - check the code
+ * @argc: an int
+ * @argv: an array
+ * Return: an integer
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
 	int i;
 	int j;
-	int sum = 0;
+	int add = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 	}
-
 	else
 	{
 		for (i = 1; i < argc; i++)
 		{
 			j = atoi(argv[i]);
-
 			if (isdigit(j) != 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += j;
-			}
+			j = atoi(argv[i]);
+			add += j;
 		}
-		printf("%d\n", sum);
+		printf("%d\n", add);
 	}
 	return (0);
 }
