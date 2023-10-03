807364cba180fe303f39dc3ef0754466be6138dd
Badu Duedu Maxwell <102438296+BaduDueduMaxwell@users.noreply.github.com>
2023-10-03 14:19:45 +0000

    task 4:fixed error condition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,39 +2,40 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/**
- * main - adds positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success and 1 on error
+/**                                                                                                 
+ * main - adds positive numbers                                                                     
+ * @argc: argument count                                                                            
+ * @argv: argument vector                                                                           
+ * Return: 0 on success and 1 on error                                                              
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int sum = 0;
+        int i;
+        int j;
+        int sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			j = atoi(argv[i]);
+        if (argc == 1)
+        {
+                printf("0\n");
+        }
 
-			if (!isdigit(j) != '\0')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum += j;
-			}
-		}
-		printf("%d\n", sum);
-	}
-	return (0);
+        else
+        {
+                for (i = 1; i < argc; i++)
+                {
+                        j = atoi(argv[i]);
+
+                        if (!isdigit(j) != '\0')
+                        {
+                                printf("Error\n");
+                                return (1);
+                        }
+                        else
+                        {
+                                sum += j;
+                        }
+                }
+                printf("%d\n", sum);
+        }
+        return (0);
 }
