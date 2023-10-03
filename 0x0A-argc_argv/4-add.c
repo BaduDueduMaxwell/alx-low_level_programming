b4489d261bcd4a19f763778537a723066010dbd2
kode <duedumaxwell43@gmail.com>
2023-10-03 07:11:58 -0700

    task 4:add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,32 +10,31 @@
  */
 int main(int argc, char *argv[])
 {
-        int i;
-        int j;
-        int sum = 0;
+	int i;
+	int j;
+	int sum = 0;
 
-        if (argc == 1)
-        {
-                printf("0\n");
-        }
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = atoi(argv[i]);
 
-        else
-        {
-                for (i = 1; i < argc; i++)
-                {
-                        j = atoi(argv[i]);
-
-                        if (!isdigit(j) != '\0')
-                        {
-                                printf("Error\n");
-                                return (1);
-                        }
-                        else
-                        {
-                                sum += j;
-                        }
-                }
-                printf("%d\n", sum);
-        }
-        return (0);
+			if (!isdigit(j) != '\0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += j;
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
 }
