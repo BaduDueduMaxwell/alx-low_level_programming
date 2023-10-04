409bb8bef3718f96e0891ea31e06755b9c71e7c4
kode <duedumaxwell43@gmail.com>
2023-10-04 15:42:27 -0700

    task 5:minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,6 +2,47 @@
 #include <stdlib.h>
 
 /**
+ * calculateMinCoins - calculates the minimum number of coins required
+ * @cents: the amount in cents
+ * Return: the minimum number of coins
+ */
+int calculateMinCoins(int cents)
+{
+	int coins25  = 0, coins10 = 0;
+	int coins5 = 0, coins2 = 0, coins1 = 0;
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			coins25++;
+			cents -= 25;
+		}
+		else if (cents >= 10)
+		{
+			coins10++;
+			cents -= 10;
+		}
+		else if (cents >= 5)
+		{
+			coins5++;
+			cents -= 5;
+		}
+		else if (cents >= 2)
+		{
+			coins2++;
+			cents -= 2;
+		}
+		else if (cents >= 1)
+		{
+			coins1++;
+			cents -= 1;
+		}
+	}
+	return (coins25 + coins10 + coins5 + coins2 + coins1);
+}
+
+/**
  * main - prints the minimum number of coins to change
  * @argc: argument count
  * @argv: argument vector
@@ -25,38 +66,9 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		int coins25  = 0, coins10 = 0;
-		int coins5 = 0, coins2 = 0, coins1 = 0;
+		int minCoins = calculateMinCoins(cents);
 
-		while(cents > 0)
-		{
-			if (cents >= 25)
-			{
-				coins25++;
-				cents -= 25;
-			}
-			else if (cents >= 10)
-			{
-				coins10++;
-				cents -= 10;
-			}
-			else if (cents >= 5)
-			{
-				coins5++;
-				cents -= 5;
-			}
-			else if (cents >= 2)
-			{
-				coins2++;
-				cents -= 2;
-			}
-			else if (cents >= 1)
-			{
-				coins1++;
-				cents -= 1;
-			}
-		}
-		printf("%d\n", coins25 + coins10 + coins5 + coins2 + coins1);
+		printf("%d\n", minCoins);
 	}
 	return (0);
 }
